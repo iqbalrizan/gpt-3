fd8f95010057aa15d547557202c48c9638fa1a9b
iqbalrizan <iqbalrizan@gmail.com>
2023-02-21 20:54:36 +0700

    initial project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
+import Brand from './components/brand/Brand';
+import WhatGPT3 from './components/whatGPT3/WhatGPT3';
+import CTA from './components/CTA/CTA';
+import Blog from './components/Blog/Blog';
+import Possibility from './components/Possibility/Possibility';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__BG">
+      <Navbar />
+      <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <CTA />
+      <Possibility />
+      <Blog />
+      <Footer />
     </div>
   );
 }
